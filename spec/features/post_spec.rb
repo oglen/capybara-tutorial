330b57f684bb47514121341caa06f22305881404
linfang <lin.fang@rea-group.com>
2016-08-05 14:13:25 +0800

    switch remote repo.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,21 +1,92 @@
 require 'rails_helper'
 
+sleepSeconds = 0
+
 RSpec.feature 'post feature', type: :feature do
   scenario 'create new post' do
     visit '/posts'
-    sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
+    sleep(sleepSeconds)
+
     click_link('New Post')
     fill_in('post_title', with: 'my first post')
-    sleep(1)
+    sleep(sleepSeconds)
     fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
+    sleep(sleepSeconds)
     click_button('Create Post')
-    sleep(1)
+    sleep(sleepSeconds)
+    expect(page).to have_content('Title: my first post')
+    expect(page).to have_content('Content: I wanna be the full-stack dev')
+    sleep(sleepSeconds)
+    click_link('Back')
+
+    sleep(sleepSeconds)
+
+    click_link('New Post')
+    fill_in('post_title', with: 'my second post')
+    sleep(sleepSeconds)
+    fill_in('post_content', with: 'Scala is the best programing language in the world.')
+    sleep(sleepSeconds)
+    click_button('Create Post')
+    sleep(sleepSeconds)
+    expect(page).to have_content('Title: my second post')
+    expect(page).to have_content('Content: Scala is the best programing language in the world.')
+    sleep(sleepSeconds)
+    click_link('Back')
+
+    sleep(sleepSeconds)
+
+    click_link('Edit', match: :first)
+    sleep(sleepSeconds)
+    fill_in('post_title', with: 'my first post edited')
+    sleep(sleepSeconds)
+    fill_in('post_content', with: 'I wanna be a scala dev')
+    sleep(sleepSeconds)
+    click_button('Update Post')
+    sleep(sleepSeconds)
+    expect(page).to have_content('Title: my first post edited')
+    expect(page).to have_content('Content: I wanna be a scala dev')
+    click_link('Back')
+
+    sleep(sleepSeconds)
+
+    all('a', :text => 'Edit')[1].click
+    fill_in('post_title', with: 'my second post edited')
+    sleep(sleepSeconds)
+    fill_in('post_content', with: 'Scala is the best programing language in the universe.')
+    sleep(sleepSeconds)
+    click_button('Update Post')
+    sleep(sleepSeconds)
+    expect(page).to have_content('Title: my second post edited')
+    expect(page).to have_content('Content: Scala is the best programing language in the universe.')
+
+    sleep(sleepSeconds)
+
     visit '/posts'
-    sleep(1)
-    expect(page).to have_content('my first post')
+    sleep(sleepSeconds)
+    expect(page).to have_content('my first post edited')
+    expect(page).to have_content('I wanna be a scala dev')
+    expect(page).to have_content('my second post edited')
+    expect(page).to have_content('Scala is the best programing language in the universe.')
+    sleep(sleepSeconds)
+
+    click_link('Destroy', match: :first)
+    sleep(sleepSeconds)
+    page.driver.browser.switch_to.alert.accept
+    sleep(sleepSeconds)
+    expect(page).to have_content('Post was successfully destroyed.')
+    expect(page).not_to have_content('my first post edited')
+    expect(page).not_to have_content('I wanna be a scala dev')
+
+    sleep(sleepSeconds)
+
+    visit '/posts'
+    expect(page).not_to have_content('Post was successfully destroyed.')
+    click_link('Destroy', match: :first)
+    sleep(sleepSeconds)
+    page.driver.browser.switch_to.alert.accept
+    sleep(sleepSeconds)
+    expect(page).to have_content('Post was successfully destroyed.')
+    expect(page).not_to have_content('my second post edited')
+    expect(page).not_to have_content('Scala is the best programing language in the universe.')
   end
 end
